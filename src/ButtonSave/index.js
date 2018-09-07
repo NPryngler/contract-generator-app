623c9ab4d83c5d7e2557f5d36fd51a1657951e6a
Natasha Pryngler <natasha.pryngler@gmail.com>
2018-09-07 09:55:27 -0400

    finished signature section. Started working on the save button

---
+++ src/ButtonSave/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./style.css";
+
+
+class Button extends Component {
+    render() {
+
+        return (
+        <button
+            className="create-contract"
+            type="submit">
+            Save Contract
+        </button>)
+    }
+}
+
+export default Button;
--- src/ContractForm/index.js
+++ src/ContractForm/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./style.css";
 import ContractView from "../ContractView";
+import ButtonSave from "../ButtonSave";
 import moment from "moment";
 import numeral from "numeral";
 
 class ContractForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             freelancerName: "Developer's complete name ",
@@ -62,7 +63,15 @@ class ContractForm extends Component {
         })
     }
 
-    
+    handleChangeExecutionDate = (event) => {
+        this.setState({
+            executionDate: event.target.value,
+        })
+    }
+
+    saveChanges
+
+
 
 
     render() {
@@ -75,86 +84,110 @@ class ContractForm extends Component {
                             <form onClick={this.handleSubmit}>
 
                                 <h4 className="input-title">Freelancer's complete name</h4>
-                                <input 
-                                className="title-input" 
-                                placeholder="Freelancer complete name" 
-                                freelancerName={this.state.value} 
-                                onChange={this.handleChangeFreela}>
+                                <input
+                                    className="title-input"
+                                    placeholder="Freelancer complete name"
+                                    freelancerName={this.state.value}
+                                    onChange={this.handleChangeFreela}>
                                 </input>
 
                                 <h4 className="input-title">Client's complete name</h4>
-                                <input 
-                                className="title-input" 
-                                placeholder="Client's complete name" 
-                                clientName={this.state.value} 
-                                onChange={this.handleChangeClient}>
+                                <input
+                                    className="title-input"
+                                    placeholder="Client's complete name"
+                                    clientName={this.state.value}
+                                    onChange={this.handleChangeClient}>
                                 </input>
 
                                 <h4 className="input-title">Service description</h4>
-                                <input 
-                                className="title-input" 
-                                placeholder="service description" 
-                                serviceDescription={this.state.value} 
-                                onChange={this.handleChangeService}>
+                                <input
+                                    className="title-input"
+                                    placeholder="service description"
+                                    serviceDescription={this.state.value}
+                                    onChange={this.handleChangeService}>
                                 </input>
-                                
+
                                 <h4 className="input-title">Service due date</h4>
-                                <input 
-                                className="title-input"
-                                type="date" 
-                                placeholder="due date" 
-                                serviceDueDate={this.state.value} 
-                                onChange={this.handleChangeServiceDueDate}>
+                                <input
+                                    className="title-input"
+                                    type="date"
+                                    placeholder="due date"
+                                    serviceDueDate={this.state.value}
+                                    onChange={this.handleChangeServiceDueDate}>
                                 </input>
-                                
+
                                 <h4 className="input-title">Service fee</h4>
-                                <input 
-                                className="title-input"
-                                type="number" 
-                                placeholder="$" 
-                                serviceFee={this.state.value} 
-                                onChange={this.handleChangeServiceFee}>
+                                <input
+                                    className="title-input"
+                                    type="number"
+                                    placeholder="$"
+                                    serviceFee={this.state.value}
+                                    onChange={this.handleChangeServiceFee}>
                                 </input>
-                                
+
                                 <h4 className="input-title">Payment conditions</h4>
-                                <input 
-                                className="title-input"
-                                type="text" 
-                                placeholder="specify the payment conditions" 
-                                paymentConditions={this.state.value} 
-                                onChange={this.handleChangePaymentConditions}>
+                                <input
+                                    className="title-input"
+                                    type="text"
+                                    placeholder="specify the payment conditions"
+                                    paymentConditions={this.state.value}
+                                    onChange={this.handleChangePaymentConditions}>
                                 </input>
 
                                 <h4 className="input-title">Location</h4>
-                                <input 
-                                className="title-input"
-                                type="text" 
-                                placeholder="Which State are you based?" 
-                                stateLocation={this.state.value} 
-                                onChange={this.handleChangeStateLocation}>
+                                <input
+                                    className="title-input"
+                                    type="text"
+                                    placeholder="Which State are you based?"
+                                    stateLocation={this.state.value}
+                                    onChange={this.handleChangeStateLocation}>
                                 </input>
-                                <button className="save-changes" onclick={this.saveChanges}>Save Changes</button>
-                                <button 
-                                className="create-contract"
-                                type="submit"
-                                >Create Contract</button>
+                                <label>Early termination
+                                    <select>
+                                        <option>pro rata payment</option>
+                                        <option>liquidated damages</option>
+                                    </select>
+                                </label>
+                                <input
+                                    className="title-input"
+                                    type="date"
+                                    placeholder="execution date"
+                                    executionDate={this.state.value}
+                                    onChange={this.handleChangeExecutionDate}>
+                                </input>
+                                <ButtonSave
+                                freelancerNameProps={this.state.freelancerName}
+                                clientNameProps={this.state.clientName}
+                                serviceDescriptionProps={this.state.serviceDescription}
+                                serviceDueDateProps={this.state.serviceDueDate}
+                                serviceFeeProps={this.state.serviceFee}
+                                paymentConditionsProps={this.state.paymentConditions}
+                                stateLocationProps={this.state.stateLocation}
+                                executionDateProps={this.state.executionDate} 
+                                />
+                                <button
+                                    className="create-contract"
+                                    type="submit">
+                                    Create Contract
+                                </button>
+
 
                             </form>
                         </div>
                     </div>
-                
+
                 </div>
-                
+
                 <div className="contract-view-wrapper">
-                    <ContractView 
-                    freelancerNameProps={this.state.freelancerName}                     
-                    clientNameProps={this.state.clientName} 
-                    serviceDescriptionProps={this.state.serviceDescription} 
-                    serviceDueDateProps={this.state.serviceDueDate} 
-                    serviceFeeProps={this.state.serviceFee}
-                    paymentConditionsProps={this.state.paymentConditions}
-                    stateLocationProps={this.state.stateLocation}
-                    
+                    <ContractView
+                        freelancerNameProps={this.state.freelancerName}
+                        clientNameProps={this.state.clientName}
+                        serviceDescriptionProps={this.state.serviceDescription}
+                        serviceDueDateProps={this.state.serviceDueDate}
+                        serviceFeeProps={this.state.serviceFee}
+                        paymentConditionsProps={this.state.paymentConditions}
+                        stateLocationProps={this.state.stateLocation}
+                        executionDateProps={this.state.executionDate}
                     />
                 </div>
             </div>
